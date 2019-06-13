3c4c55f6050d94210fbc4c56ff7ca29a3683cb40
juanhart1 <juanhart1@gmail.com>
2019-06-13 03:24:30 -0400

    Added styling to popup.html and functionality to stop timer from continuously going off

--- popup.js
+++ popup.js
@@ -1,15 +1,40 @@
 window.onload = function() {
-  let counter = 2;
+  //the timer variable stores the inputted minute value
+  let timer = prompt('How many minutes between breaks?');
+  //the convertedMinutes variable stores the value of the timer converted from minutes to milliseconds
+  let convertedMinutes = minutesToMilliseconds(timer);
+  //turnOnButton variable stores the element with the id of #turnOn
   const turnOnButton = document.querySelector('#turnOn');
+  //the turnTimeOff variable is initially unassigned
+  //reassigned when the timer is turned on
+  //stores the id returned from setInterval
+  //allows us to pass this value to the clearInterval to stop the function from continuously running
+  let turnTimerOff;
+  //this event handler triggers the takeBreak function
   turnOnButton.addEventListener('click', function() {
-      takeBreak(5000);
+    turnTimerOff = takeBreak(convertedMinutes);
   })
-
+  //turnOffButton variable stores the element with the id of #turnOff
+  const turnOffButton = document.querySelector('#turnOff');
+  turnOffButton.addEventListener('click', function (){
+    //We pass clearInterval the id returned from the setInterval invocation
+    clearInterval(turnTimerOff);
+  })
+  //takeBreak function invokes setInterval and passes an alert as a function and a time to indicate how often to execute it
   function takeBreak(time) {
-    setInterval(function() {alert("Take a break!")}, time)
+    //We have to return the id returned from the invocation of setInterval
+    return setInterval(function() {alert("Take a break!")}, time)
+  }
+}
+  //minutesToMilliseconds converts minutes to milliseconds
+  function minutesToMilliseconds (minutes){
+    return minutes * 60000;
   }
 
-  // const turnOffButton = document.querySelector('#turnOff');
-  // turnOffButton.addEventListener('click', function() {clearInterval(takeBreak)})
-
-}
+//How can I take input from HTML???
+  //I'll need to create input field in HTML first
+  //Select that input field via id
+  //Save selected input to variable
+  //Use .select method on variable
+  //Use document.execCommand method and pass it string of 'copy'
+  //Should be able to access it at variable.value
